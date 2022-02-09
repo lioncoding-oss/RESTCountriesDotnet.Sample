2491875c9414bd49860ea7758abcf1c733f2c7fb
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-02-09 13:15:31 +0100

    refactor: update main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RESTCountries.Services;
+
+// Get all countries
+Console.WriteLine("... All counties ...");
+var countries = await RESTCountriesAPI.GetAllCountriesAsync();
+countries.ForEach(country => Console.WriteLine(country.Name));
+
+
+// Search by regional bloc
+Console.WriteLine("... All counties in African Union ...");
+var countriesInAU = await RESTCountriesAPI.GetCountriesByRegionalBlocAsync("AU"); // "AU" stands for "African Union"
+countriesInAU.ForEach(country => Console.WriteLine(country.Name));
+
+
+// Search by ISO 4217 currency code
+Console.WriteLine("... Countries using the currency Euro ...");
+var countriesUsingEur = await RESTCountriesAPI.GetCountriesByCurrencyCodeAsync("eur");
+countriesUsingEur.ForEach(country => Console.WriteLine(country.Name));
+
+
+// Names of countries in Spanish language
+Console.WriteLine("... Countries name using Spanish language ...");
+var allCountries = await RESTCountriesAPI.GetAllCountriesAsync();
+List<string> countriesInSpanish = allCountries.Select(c => c.Translations.Fr).ToList();
+countriesInSpanish.ForEach(country => Console.WriteLine(country));
+
+// Name of Europe's countries in French language
+Console.WriteLine("... Europe counties name in French language ...");
+var europeCountries = countries.Where(c => c.Region.Equals("Europe")).OrderBy(c => c.Name).ToList(); ;
+List<string> europeCountriesInFrench = europeCountries.Select(c => c.Translations.Fr).ToList();
+europeCountriesInFrench.ForEach(country => Console.WriteLine(country));
